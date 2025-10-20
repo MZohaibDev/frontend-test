664d3ce3e86dfc865d9b053bade099c4cfea76ca
MZohaibDev <163574727+MZohaibDev@users.noreply.github.com>
2025-10-20 13:14:54 +0500

    add start stop buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import { Carousel } from "./Components/Carousel/Carousel.jsx";
 function App() {
   return (
     <>
+      <h1>Frontend Test </h1>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "space-around",
+          margin: "20px",
+          columnGap: "20px",
+        }}
+      >
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
       <Carousel />
     </>
   );
